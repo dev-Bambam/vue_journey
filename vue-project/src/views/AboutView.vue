2561fb22af7ee6f9dd151a27e59de4264688976d
dev-Bambam <ayogood18@gmail.com>
2024-08-21 15:39:02 +0100

    trying some stuff out

--- vue-project/src/views/AboutView.vue
+++ vue-project/src/views/AboutView.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div>
+      <h2>Price: ${{ price }}</h2>
+      <label for="discount">Discount: {{ discount }}%</label>
+      <input type="range" id="discount" v-model="discount" min="0" max="100" />
+      <h3>Discounted Price: ${{ discountedPrice }}</h3>
+    </div>
   </div>
 </template>
 
@@ -13,3 +18,18 @@
   }
 }
 </style>
+
+<script setup>
+import { ref, computed } from 'vue'
+
+// Initial price
+const price = ref(100)
+
+// Discount percentage
+const discount = ref(0)
+
+// Computed property to automatically calculate discounted price
+const discountedPrice = computed(() => {
+  return price.value * (1 - discount.value / 100)
+})
+</script>
